d0e9bd69924a30c1f9456637278b2a64ae480487
Eren Akbaba <eakbaba@amazon.com>
2022-11-06 23:57:15 +0300

    change message ea4

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World EA_3!'));
+app.get('/',function(req,res) {
+    res.sendFile('index.html');
+  });
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
