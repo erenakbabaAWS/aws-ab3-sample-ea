669427772221e3405e3b0d80675dedc7a7b08310
Eren Akbaba <eakbaba@amazon.com>
2023-03-22 00:21:22 +0300

    change message1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World EA_3!'));
+app.get('/', (req, res) => res.send('Hello World from ErenAkbaba!-1'));
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'./index.html'));
 });
